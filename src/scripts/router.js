2b446a4ac3d62ce0c6874973afc529f11d89d795
Requals <131925306+Requals@users.noreply.github.com>
2023-05-27 22:10:36 -0400

    Fix error page briefly showing up (Yes I know 404 is an HTTP status)

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -3,75 +3,77 @@
 //
 
 class Router {
-    static #currentRouter;
+	static #currentRouter;
 
-    // Initiates the router.
-    static initiateRouter() {
-        // If no RouterController is found, create an instance.
-        if(!Router.#currentRouter) {
-            Router.#currentRouter = new RouterController();
-        }
-        return Router.#currentRouter;
-    }
+	// Initiates the router.
+	static initiateRouter() {
+		// If no RouterController is found, create an instance.
+		if (!Router.#currentRouter) {
+			Router.#currentRouter = new RouterController();
+		}
+		return Router.#currentRouter;
+	}
 }
 
 class RouterController {
+	createDocument() {
+		const routes = this.routes;
+		const base = this.base;
 
-    createDocument() {
+		// Notifies the user of an error.
+		function notifyError(error) {
+			if (typeof error === "string") {
+				document.querySelector(".error").innerHTML = error;
+				throw new Error(error);
+			}
+			document.querySelector(".error").innerHTML = `Error: ${error.toString()}`;
+		}
 
-        const routes = this.routes;
-        const base = this.base;
+		// Checks for an error.
+		function findInitErrors() {
+			if (!routes || !base) {
+				notifyError("RouterController: No route/base provided!");
+			}
+		}
 
-        // Notifies the user of an error.
-        function notifyError(error) {
-            if(typeof error === "string") {
-                document.querySelector(".error").innerHTML = error
-                throw new Error(error)
-            }
-            document.querySelector(".error").innerHTML = error.toString();
-        }
+		findInitErrors();
 
-        // Checks for an error.
-        function findInitErrors() {
-            if(!routes || !base) {
-                notifyError("RouterController: No route/base provided!")
-            }
-        }
+		// Retrieves the route to a path.
+		function getRoute() {
+			// Get the URL without fragments or query strings.
+			let url = window.location.origin + window.location.pathname;
+			let urlRoute = url.replace(base, "");
 
-        findInitErrors();
+			// If the route is not found, 404.
+			if (!Object.keys(routes).includes(urlRoute)) {
+				document.body.innerHTML = `
+					<h1>Oops!</h1>
+    				<samp class="message">Error: <samp class="error"></samp></samp>
+				`
+				notifyError("404 Not Found");
+			}
 
-        // Retrieves the route to a path.
-        function getRoute() {
-            // Get the URL without fragments or query strings.
-            let url = window.location.origin + window.location.pathname;
-            let urlRoute = url.replace(base,"");
+			let result = routes[urlRoute];
 
-             // If the route is not found, 404.
-             if(!Object.keys(routes).includes(urlRoute)){
-               notifyError("404 Not Found")
-             }
+			return result;
+		}
 
-            let result = routes[urlRoute]
-            
-            return result;
-        }
+		// Replaces the current document with another document.
+		function createDocument(route) {
+			// Evil magic. Fetches the page and uses DOMParser to parse a document.
+			fetch(`src/pages/${route}`)
+				.then((response) => response.text())
+				.then((text) => {
+					const parser = new DOMParser();
+					let htmlDoc = parser.parseFromString(text, "text/html");
+					document.body.innerHTML = htmlDoc.body.innerHTML;
+					document.head.innerHTML = htmlDoc.head.innerHTML;
+				});
+		}
 
-        // Replaces the current document with another document.
-        function createDocument(route) {
-            // Evil magic. Fetches the page and uses DOMParser to parse a document.
-            fetch(`src/pages/${route}`).then(response => response.text()).then(
-                text => {
-                    const parser = new DOMParser();
-                    let htmlDoc = parser.parseFromString(text,"text/html");
-                    document.body.innerHTML = htmlDoc.body.innerHTML;
-                    document.head.innerHTML = htmlDoc.head.innerHTML;
-                }
-            );
-        }
-        
-        // Finally create the document. Retrieves the route, retrieves the document, then creates the document.
-        createDocument(getRoute())
-    }
+		// Finally create the document. Retrieves the route, retrieves the document, then creates the document.
+		createDocument(getRoute());
+	}
 }
 
 //
@@ -84,11 +86,11 @@ const router = Router.initiateRouter();
 
 // For live server stuff
 // router.base = "https://127.0.0.1/404.html"
-router.base = "https://requals.github.io"
+router.base = "https://requals.github.io";
 
 // Routing "requests" to pages.
 router.routes = {
-    "/about": "about.html",
-    "/projects": "projects.html"
-}
+	"/about": "about.html",
+	"/projects": "projects.html",
+};
 router.createDocument();
